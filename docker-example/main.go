da0983926a0e3d1f65c827507f2385be4fc073b5
hisen <passer-by-sen@qq.com>
2025-05-11 17:54:17 +0800

    部署web项目到docker：设置外部settings.yaml文件

--- docker-example/main.go
+++ docker-example/main.go
@@ -1,11 +1,33 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"gopkg.in/yaml.v3"
+	"log"
+	"os"
+)
+
+type Conf struct {
+	System struct {
+		Name string `yaml:"name"`
+	} `yaml:"system"`
+}
 
 func main() {
 	r := gin.Default()
+
+	byteData, err := os.ReadFile("settings.yaml")
+	if err != nil {
+		log.Fatal(err)
+	}
+	var conf Conf
+	err = yaml.Unmarshal(byteData, &conf)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	r.GET("/", func(context *gin.Context) {
-		context.String(200, "看到消息就说明部署成功了")
+		context.JSON(200, gin.H{"code": 0, "msg": "看到消息就说明部署成功了", "data": gin.H{"name": conf.System.Name}})
 	})
 	r.Run(":5000")
 }
